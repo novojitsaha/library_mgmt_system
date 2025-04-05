bfe835dfe19f04f756325049833bf3412522d499
Novojit Saha <n.saha@student.utwente.nl>
2025-04-06 01:13:08 +0200

    - Add searching books based on ID and author - Add remove books based on ID

--- src/main/java/com/crescendo/library/service/LibraryService.java
+++ src/main/java/com/crescendo/library/service/LibraryService.java
@@ -2,7 +2,6 @@ package com.crescendo.library.service;
 
 import com.crescendo.library.model.Book;
 import com.crescendo.library.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -10,8 +9,11 @@ import java.util.*;
 @Service
 public class LibraryService {
 
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+
+    public LibraryService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
 
     public Book addBook(Book book) {
@@ -23,13 +25,24 @@ public class LibraryService {
     }
 
     public Optional<Book> getBookById(Long id) {
-
         return bookRepository.findById(id);
+    }
+
+
+    public Iterable<Book> getBookByTitle(String title) {
+        return bookRepository.findByTitle(title);
+    }
+
+
+    public Iterable<Book> getBookByAuthor(String author) {
+        return bookRepository.findByAuthor(author);
     }
 
     public void removeBookById(Long id) {
         bookRepository.deleteById(id);
     }
+
+
 
 
 }
