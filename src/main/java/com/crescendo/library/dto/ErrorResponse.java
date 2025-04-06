2abce53478450881fa74cb4ecf834e0ddfb20d69
Novojit Saha <n.saha@student.utwente.nl>
2025-04-06 02:54:33 +0200

    Add ErrorResponse DTO

---
+++ src/main/java/com/crescendo/library/dto/ErrorResponse.java
@@ -0,0 +1,40 @@
+package com.crescendo.library.dto;
+
+import java.util.Date;
+
+public class ErrorResponse {
+
+    private int status;
+    private String error;
+    private String message;
+    private Date timestamp;
+    private String path;
+
+    public ErrorResponse(int status, String error, String message, String path, Date timestamp) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
