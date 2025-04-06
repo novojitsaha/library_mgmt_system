d838695a269fac0ccb8bfe09e557231fcf06264c
Novojit Saha <n.saha@student.utwente.nl>
2025-04-06 05:36:47 +0200

    Add @OpenAPIDefinition for better swagger documentation

--- src/main/java/com/crescendo/library/LibraryApplication.java
+++ src/main/java/com/crescendo/library/LibraryApplication.java
@@ -1,8 +1,16 @@
 package com.crescendo.library;
-
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Library Management API",
+				version = "1.0",
+				description = "API for managing books in the library."
+		)
+)
 @SpringBootApplication
 public class LibraryApplication {
 
