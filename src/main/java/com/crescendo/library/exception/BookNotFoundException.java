00268653894bc6cd7ec62e1f4eca5b6e49a071bf
Novojit Saha <n.saha@student.utwente.nl>
2025-04-06 04:15:23 +0200

    Update BookNotFoundException message

--- src/main/java/com/crescendo/library/exception/BookNotFoundException.java
+++ src/main/java/com/crescendo/library/exception/BookNotFoundException.java
@@ -2,6 +2,6 @@ package com.crescendo.library.exception;
 
 public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException(SearchTypes searchType, Object value) {
-        super(String.format("Book with %s %s not found", searchType.name().toLowerCase(), value.toString()));
+        super(String.format("Book with %s %s cannot be found!", searchType.name().toLowerCase(), value.toString()));
     }
 }
